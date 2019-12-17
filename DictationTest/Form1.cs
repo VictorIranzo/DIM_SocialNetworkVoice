b0e12b34f4eade20c170caa3ff42147b0f7d6a0b
Víctor Iranzo <viranzo96@gmail.com>
2019-12-17 21:30:47 +0100

    Implementación de gramática para escribir comentarios.

--- DictationTest/Form1.cs
+++ DictationTest/Form1.cs
@@ -1,6 +1,8 @@
 ﻿namespace DictationTest
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Speech.Recognition;
     using System.Speech.Synthesis;
     using System.Windows.Forms;
@@ -9,6 +11,7 @@
     {
         private SpeechRecognitionEngine speechRecognizer = new SpeechRecognitionEngine();
         private SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        private Grammar grammar;
 
         public Form1()
         {
@@ -17,7 +20,7 @@
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Grammar grammar = new DictationGrammar();
+            grammar = this.CreateGrammar();
             speechRecognizer.SetInputToDefaultAudioDevice();
             speechRecognizer.UnloadAllGrammars();
             speechRecognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", 60);
@@ -25,22 +28,109 @@
             speechRecognizer.LoadGrammar(grammar);
             speechRecognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
 
-            speechRecognizer.InitialSilenceTimeout = TimeSpan.FromSeconds(3);
-            speechRecognizer.BabbleTimeout = TimeSpan.FromSeconds(1);
-            speechRecognizer.EndSilenceTimeout = TimeSpan.FromSeconds(1);
-            speechRecognizer.EndSilenceTimeoutAmbiguous = TimeSpan.FromSeconds(0.5);
-
             speechRecognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        internal Grammar CreateGrammar()
+        {
+            GrammarBuilder addCommentGrammar = this.CreateAddCommentGrammar();
+            GrammarBuilder deleteCommentGrammar = this.CreateDeleteCommentGrammar();
+
+            Choices choices = new Choices();
+            choices.Add(addCommentGrammar);
+            choices.Add(deleteCommentGrammar);
+
+            Grammar grammar = new Grammar(choices);
+
+            return grammar;
+        }
+
+        private GrammarBuilder CreateAddCommentGrammar()
+        {
+            GrammarBuilder write = "Escribir";
+            GrammarBuilder add = "Añadir";
+            GrammarBuilder dictate = "Dictar";
+
+            Choices alternativesNavigate = new Choices(write, add, dictate);
+
+            GrammarBuilder sentence = new GrammarBuilder(alternativesNavigate);
+
+            GrammarBuilder comment = "comentario";
+            sentence.Append(comment);
+
+            return sentence;
+        }
+
+        private GrammarBuilder CreateDeleteCommentGrammar()
+        {
+            GrammarBuilder deleteComment = "Borrar comentario";
+
+            return deleteComment;
         }
 
         private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
             SemanticValue semantics = e.Result.Semantics;
-
             string rawText = e.Result.Text;
-            RecognitionResult result = e.Result;
+            speechSynthesizer.Speak(rawText);
 
-            this.dictationTextBox.Text += rawText;
+            if (rawText.Equals("Borrar comentario"))
+            {
+                this.dictationTextBox.Text = string.Empty;
+            }
+
+            IEnumerable<string> writeCommands = new List<string>() { "Escribir", "Añadir", "Dictar" };
+            if (writeCommands.Any(s => rawText.Contains(s)))
+            {
+                speechRecognizer.UnloadAllGrammars();
+                speechRecognizer.SpeechRecognized -= new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                speechSynthesizer.Speak("Comienza dictado");
+
+                speechRecognizer.LoadGrammar(new DictationGrammar());
+
+                speechRecognizer.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechHypothesized);
+            }
+        }
+
+        private void EndDictation()
+        {
+            speechRecognizer.UnloadAllGrammars();
+            speechRecognizer.SpeechHypothesized -= new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechHypothesized);
+
+            speechSynthesizer.Speak("Fin dictado");
+
+            speechRecognizer.LoadGrammar(grammar);
+
+            speechRecognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+        }
+
+        private void recognizer_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            if (e.Result.Confidence < 0.6)
+            {
+                return;
+            }
+
+            SemanticValue semantics = e.Result.Semantics;
+            string rawText = e.Result.Text;
+
+            IEnumerable<string> stopDictationCommands = new List<string>() { "fin dictado", "fin", "dictado" };
+            if (stopDictationCommands.Any(s => rawText.Contains(s)))
+            {
+                this.EndDictation();
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.dictationTextBox.Text))
+            {
+                this.dictationTextBox.Text = rawText;
+            }
+            else
+            {
+                this.dictationTextBox.Text += " " + rawText;
+            }
         }
     }
 }
